4c8dbe74fd043c48a7749ed9c353e08e1bb82d9c
alessandro.santos <alessandro.santos@bemobi.com.br>
2017-07-21 17:54:24 -0300

    first commit

---
+++ src/main/java/com/bemobi/hackpuc/paygateway/PayGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.bemobi.hackpuc.paygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayGatewayApplication.class, args);
+	}
+}
